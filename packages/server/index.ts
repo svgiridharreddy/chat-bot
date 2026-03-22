60b3ece9dbb3248473f715ea497a493575f8a60a
svgiridharreddy <giridhar.reddy1992@gmail.com>
2026-03-22 22:56:06 +0530

    connecting the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,6 +8,10 @@ app.get("/",(req: Request,res: Response) => {
     res.send("Hello world")
 })
 
+app.get("/api/hello",(req: Request,res: Response) => {
+    res.json({message: "Hello world"})
+})
+
 app.listen(port,() => {
     console.log(`Server is running on http://localhost:${port}`)
 })
