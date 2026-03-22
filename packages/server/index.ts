c099c8edb7384db5932ffab45849828830c4406f
svgiridharreddy <giridhar.reddy1992@gmail.com>
2026-03-22 22:34:30 +0530

    creating the frontend

---
+++ packages/server/index.ts
@@ -0,0 +1,16 @@
+import type { Request, Response } from "express";
+import express from 'express';
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get("/",(req: Request,res: Response) => {
+    res.send("Hello world")
+})
+
+app.listen(port,() => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
+
+
+
