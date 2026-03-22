b323540eeee142058885af5789bf0fc97b40643f
svgiridharreddy <giridhar.reddy1992@gmail.com>
2026-03-22 23:08:48 +0530

    running both frontend and backend together using concurrently

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrently from 'concurrently';
+
+concurrently([
+    {
+        name: "Server",
+        command: 'bun run dev',
+        prefixColor: 'cyan',
+        cwd: "packages/server"
+    },
+    {
+        name: "Client",
+        command: 'bun run dev',
+        prefixColor: 'green',
+        cwd: "packages/client"
+    },
+])
