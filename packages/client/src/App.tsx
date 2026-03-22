b7355008ddd0f3c27f8c6c1243ff71df8e3e9a4e
svgiridharreddy <giridhar.reddy1992@gmail.com>
2026-03-23 00:15:35 +0530

    setting up Husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -9,7 +9,12 @@ function App() {
       .then((data) => setMessage(data.message));
   }, []);
 
-  return <p className="text-3xl font-bold p-4">{message}</p>;
+  return (
+    <div>
+      <p className="text-3xl font-bold p-4">{message}</p>
+      <button>Click me</button>
+    </div>
+  );
 }
 
 export default App;
